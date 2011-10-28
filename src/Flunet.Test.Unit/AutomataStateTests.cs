a8372dba227d7536edf1ef357aa40fd2b7faa905
Elad Zelingher <dark_leizar@walla.com>
2011-10-28 14:27:25 +0200

    Renaming project to Flunet instead of Fluent

--- src/Flunet.Test.Unit/AutomataStateTests.cs
+++ src/Flunet.Test.Unit/AutomataStateTests.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Flunet.Automata;
+﻿using Flunet.Automata;
 using Flunet.Automata.Interfaces;
 using NUnit.Framework;
 
-namespace Fluent.Test.Unit
+namespace Flunet.Test.Unit
 {
     [TestFixture]
     public class AutomataStateTests
