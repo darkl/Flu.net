dc4ac5e7164ed44806ff27b448c7677b2a56b468
Elad Zelingher <dark_leizar@walla.com>
2011-10-28 15:08:00 +0200

    Adding tests for AutomataState.GetEnumerator().

--- src/Flunet.Test.Unit/AutomataStateTests.cs
+++ src/Flunet.Test.Unit/AutomataStateTests.cs
@@ -1,4 +1,7 @@
-﻿using Flunet.Automata;
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Flunet.Automata;
 using Flunet.Automata.Interfaces;
 using NUnit.Framework;
 
@@ -44,15 +47,62 @@ namespace Flunet.Test.Unit
         }
 
         [Test]
-        [ExpectedException(typeof(System.ArgumentException))]
         public void Add_AddingSameInputTwice_ThrowsException()
         {
             AutomataState<int> q1 = new AutomataState<int>("Q1", true);
             int inputToSelf = 1;
             q1.Add(inputToSelf, q1);
-            q1.Add(inputToSelf, q1);
+
+            Assert.Throws<ArgumentException>(() => q1.Add(inputToSelf, q1));
         }
 
+        [Test]
+        public void GetEnumerator_NoTransitions_NoEnumeration()
+        {
+            AutomataState<int> state = new AutomataState<int>("Root", true);
 
+            Assert.IsTrue(!state.Any());
+        }
+
+        [Test]
+        public void GetEnumerator_SingleTransition_EnumeratesTransition()
+        {
+            AutomataState<int> state = new AutomataState<int>("Root", true);
+            AutomataState<int> state1 = new AutomataState<int>("State1", true);
+
+            state.Add(1, state1);
+
+            Dictionary<int, IAutomataState<int>> exceptedEnumeration =
+                new Dictionary<int, IAutomataState<int>>()
+                    {
+                        {1, state1}
+                    };
+
+            Assert.IsTrue(state.SequenceEqual(exceptedEnumeration));
+        }
+
+        [Test]
+        public void GetEnumerator_AddingFewTransition_EnumeratesTransition()
+        {
+            AutomataState<int> state = new AutomataState<int>("Root", true);
+
+            AutomataState<int> state1 = new AutomataState<int>("State1", true);
+            AutomataState<int> state2 = new AutomataState<int>("State2", true);
+            AutomataState<int> state3 = new AutomataState<int>("State3", true);
+            
+            state.Add(1, state1);
+            state.Add(2, state2);
+            state.Add(3, state3);
+
+            Dictionary<int, IAutomataState<int>> exceptedEnumeration =
+                new Dictionary<int, IAutomataState<int>>()
+                    {
+                        {1, state1},
+                        {2, state2},
+                        {3, state3}
+                    };
+
+            Assert.IsTrue(state.SequenceEqual(exceptedEnumeration));
+        }
     }
 }
